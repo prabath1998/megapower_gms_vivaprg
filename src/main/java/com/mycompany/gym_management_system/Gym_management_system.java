68b51c62b7225ffea1abc4dd81c64660c9d2278c
prabath1998 <prabhathudayanga1998@gmail.com>
2023-01-31 14:27:55 +0530

    login implemented

---
+++ src/main/java/com/mycompany/gym_management_system/Gym_management_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gym_management_system;
+
+/**
+ *
+ * @author prabath
+ */
+public class Gym_management_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
